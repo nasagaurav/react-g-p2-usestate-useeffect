6ab1dd07f2fee2b4cfa7c5afe84931e45dc6d5d6
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-18 18:04:18 +0530

    all topics and added map to display  topics

--- src/App.js
+++ src/App.js
@@ -2,22 +2,26 @@ import React from 'react';
 import './style.css';
 
 export default function App() {
-      const [topic,settopic]= React.useState('html');
+  const [topic, settopic] = React.useState('html');
 
-      const handleChange=(e)=>{
-        settopic(e.target.value);
-      };
-  
+  const [a, seta] = React.useState([]);
+
+  const handleChange = (e) => {
+    settopic(e.target.value);
+  };
+
+  const addTopic = () => {};
+
   return (
-
     <div>
-        <input value="topic" onChange={handleChange}/>
-          <button>add topic</button>
-          <ol>
-            <li>topic name is </li>
-
-          </ol>
-
+      <input value="topic" onChange={handleChange} />
+      <button>add topic</button>
+      <ol>
+        <li>topic name is </li>
+        {a.map((item) => (
+          <li>topic name is </li>
+        ))}
+      </ol>
     </div>
   );
 }
