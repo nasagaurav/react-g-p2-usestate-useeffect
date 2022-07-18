faa145af8311a692ae7f632a5c6d94862e35d42c
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-18 17:58:47 +0530

    added control input via usestate

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './style.css';
 
 export default function App() {
+      const [topic,settopic]= React.useState('html');
 
+      const handleChange=(e)=>{
+        settopic(e.target.value);
+      };
   
   return (
 
     <div>
-        <input type="text">
+        <input value="topic" onChange={handleChange}/>
           <button>add topic</button>
           <ol>
             <li>topic name is </li>
