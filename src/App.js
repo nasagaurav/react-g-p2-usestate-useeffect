2e8808aa4241ef821264f59ee3bfc4a7ef4afb18
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-18 18:02:02 +0530

    input inserted

--- src/App.js
+++ src/App.js
@@ -2,17 +2,26 @@ import React from 'react';
 import './style.css';
 
 export default function App() {
+
      const [x,setx]= React.useState(0);
-  const p1= ()=>{ setx(x+1);};
+
+  const p1= ()=>{setx(x+1);};
+
   const p2=()=>{setx(x-1);};
 
   Const p3=()=>{setx(0);};
+
+  const changeTitle=
+  ()=>{document.Title=x;};
+
+  React.useEffect(changeTitle,[x]);
  
 
   return (
 
     <div>
-      <h1>counter</h1>
+      <input type="number" onChange={handleChange}/>
+      <h1>counter{x}</h1>
       <button onClick={p1}>+</button>
       <button onClick={p2}>-</button>
       <button onClick={p3}>0</button>
