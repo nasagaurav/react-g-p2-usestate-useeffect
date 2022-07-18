83dd1d7a164d8fcfa9347b255369e3b53d1d7aa1
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-18 18:15:18 +0530

    setx value defined

--- src/App.js
+++ src/App.js
@@ -2,7 +2,26 @@ import React from "react";
 import "./style.css";
 
 export default function App() {
+    
+        const [x,setx] = React.useState(0);
+        const [y,sety] = React.useState(0);
+
+          
+          const p1 ()=>{
+                setx(x+1)
+          }
+          const p2 ()=>{
+                setx(x-1)
+          }
+
+
   return (
- 
+
+    <div>
+    <h1>counter</h1>
+    <h2>changes made are </h2>
+    <button onClick="p1">+</button>
+    <button onClick="p2">-</button>
+    </div>
   );
 }
