f6e8050e04e9e8d51bd60441cc44e540bf774d8b
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-18 17:38:51 +0530

    button created

--- src/App.js
+++ src/App.js
@@ -2,32 +2,27 @@ import React from "react";
 import "./style.css";
 
 export default function App() {
-    
-        const [x,setx] = React.useState(0);
-        const [y,sety] = React.useState(0);
+    const p1=()=>{}
+    const p2=()=>{}
+    const p3=()=>{}
+    const p4=()=>{}
+    const p5=()=>{}
+    const p6=()=>{}
 
-          
-          const p1=()=>{
-                setx(x+1);
-          };
-          const p2=()=>{
-                setx(x-1);
-          };
-
-          const p3=()=>{
-            sety(y+1);
-          };
-          React.useEffect(p3,[x])
 
 
 
   return (
 
     <div>
-    <h1>counter{x}</h1>
-    <h2>changes made are{y} </h2>
-    <button onClick={p1}>+</button>
-    <button onClick={p2}>-</button>
+      <button onclick={p1}>click to change title1</button>
+      <button onclick={p2}>click to change title2</button>
+      <button onclick={p3}>click to change title3</button>
+      <button onclick={p4}>click to change title4</button>
+      <button onclick={p5}>click to change title5</button>
+      <button onclick={p6}>click to change title6</button>
     </div>
+
+  
   );
 }
