0f6fedd863bdd3a4ab9c625d55fa14f36ad486ea
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-18 17:52:20 +0530

    create button, defined function

--- src/App.js
+++ src/App.js
@@ -2,10 +2,21 @@ import React from 'react';
 import './style.css';
 
 export default function App() {
+     const [x,setx]= React.useState(0);
+  const p1= ()=>{ setx(x+1);};
+  const p2=()=>{setx(x-1);};
 
+  Const p3=()=>{setx(0);};
  
 
   return (
+
+    <div>
+      <h1>counter</h1>
+      <button onClick={p1}>+</button>
+      <button onClick={p2}>-</button>
+      <button onClick={p3}>0</button>
+    </div>
     
   );
 }
